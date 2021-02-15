93ddd52b00a01e162218e3283022b73b982f2b23
Raju Dhoni <32018260+Rajudhoni@users.noreply.github.com>
2021-02-15 19:44:41 +0530

    navigation menu updated

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -9,6 +9,9 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Button from '@material-ui/core/Button';
 import {Link} from 'react-router-dom';
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+
 
 
 
@@ -31,7 +34,13 @@ const useStyles = makeStyles( theme => ({
             marginBottom: "3em"
         },
         logo: {
-            height: "7em"
+            height: "8em"
+        },
+        logoContainer: {
+            padding: 0,
+            "&:hover": {
+                backgroundColor: "transparent"
+            }
         },
         tabsContainer: {
             marginLeft: "auto"
@@ -55,26 +64,89 @@ const useStyles = makeStyles( theme => ({
 const Header = () => {
     const [Value, setValue] = useState(0);
     const classes = useStyles();
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [open, setOpen] = useState(false);
 
     const handleChange = (e,value) => {
         setValue(value);
+    };
+
+    const handleClick = (e) => {
+        console.log(e.currentTarget);
+        setAnchorEl(e.currentTarget);
+        setOpen(true);
     }
+
+    const handleClose = (e) => {
+        setAnchorEl(null);
+        setOpen(false);
+    }
+
+    useEffect(()=> {
+        if(window.location.pathname === "/" && Value !== 0){
+            setValue(0);
+        } else if(window.location.pathname === "/revolution" && Value !== 1) {
+            setValue(1);
+        } else if(window.location.pathname === "/about" && Value !== 2){
+            setValue(2);
+        }else if(window.location.pathname === "/services" && Value !== 3) {
+            setValue(3);
+        }else if(window.location.pathname === "/contact" && Value !== 4) {
+            setValue(4);
+        } else if(window.location.pathname === "/estimate" &&  Value !== 5){
+            setValue(5)
+        }
+
+
+    })
     return(
         <React.Fragment>
         <ElevationScroll>
         <AppBar position="fixed" color="primary">
             <Toolbar disableGutters>
-                <img alt="logo" src={logo} className={classes.logo}  />
+                <Button component={Link} to="/" 
+                        className={classes.logoContainer}
+                        onClick={()=> setValue(0)} 
+                        disableRipple
+                >
+                    <img alt="logo" src={logo} className={classes.logo}  />
+                </Button>
                 <Tabs value={Value} className={classes.tabsContainer} onChange={handleChange}>
                     <Tab className={classes.tab} label="Home" component={Link} to="/" />
                     <Tab className={classes.tab} label="The revolution" component={Link} to="/revolution" />
                     <Tab className={classes.tab} label="About Us" component={Link} to="/about" />
-                    <Tab className={classes.tab} label="Services" component={Link} to="/services" />
+                    <Tab 
+                            aria-owns={anchorEl ? "simple-menu" : undefined}
+                            aria-haspopup={anchorEl ? "true" : undefined}
+                            onMouseOver={event => handleClick(event)}
+                            className={classes.tab} 
+                            label="Services" 
+                            component={Link} 
+                            to="/services" 
+
+                    />
                     <Tab className={classes.tab} label="Contact Us" component={Link} to="/contact" />
                 </Tabs>
-                <Button variant="contained" color="secondary" className={classes.button}>
+                <Button variant="contained" color="secondary"
+                        className={classes.button} component={Link} 
+                        to="/estimate"
+                        onClick={()=> setValue(5)}
+                >
                         Free Estimate
                 </Button>
+
+                    <Menu id="simple-menu" 
+                        anchorEl={anchorEl} 
+                        open={open} 
+                        onClose={handleClose}
+                        MenuListProps={{onMouseLeave: handleClose}}
+                    >
+                    
+                        <MenuItem onClick={()=>{handleClose(); setValue(3)}} component={Link} to="customsoftware">Custom Software Development</MenuItem>
+                        <MenuItem onClick={()=>{handleClose(); setValue(3)}} component={Link} to="mobileapps">Mobile App Development</MenuItem>
+                        <MenuItem onClick={()=>{handleClose(); setValue(3)}} component={Link} to="websites">Website Development</MenuItem>
+
+                    </Menu>
             </Toolbar>
 
 
